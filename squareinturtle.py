3e8086d2296d641a2b21dcf052afb9fa9e05ac78
yousef19-meet <yousef19@meet.mit.edu>
2017-07-24 15:50:09 +0200

    Stopping here

---
+++ squareinturtle.py
@@ -0,0 +1,30 @@
+import turtle
+
+#Insert variable here
+
+turtle.penup() #Pick up the pen so it doesn’t draw
+side_length=100
+#Move the turtle to the
+
+#position, -200, -100, on
+
+#the screen
+
+turtle.pendown() #Put the pen down to start
+
+#drawing
+
+#Draw the square:
+
+turtle.goto(0,side_length)
+
+turtle.goto(100,100)
+
+turtle.goto(100,0)
+
+turtle.goto(0,0)
+
+# ...and end it before the next line.
+
+turtle.mainloop()
+
---
+++ turtlecircle.py
@@ -0,0 +1,10 @@
+import turtle
+t = turtle.Pen()
+t.left(90)
+for x in range(180):
+    t.forward(1)
+    t.right(1)
+
+for x in range(180):
+    t.forward(1)
+    t.right(1)
